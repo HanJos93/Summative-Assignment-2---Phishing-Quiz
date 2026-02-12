8ee1a16eed0a2f7937eaea2b229143fc48d03b6f
HanJos93 <kneba.j@northeastern.edu>
2026-02-12 15:39:32 +0000

    Smoke tests + unit tests

--- test_unittests.py
+++ test_unittests.py
@@ -1,7 +1,31 @@
 import unittest #Testing framework 
-from quiz_questions import load_questions
+import re #For validating user input
+from quiz_questions import load_questions #Converts questions in csv to a dictionary
 
 class SmokeTest(unittest.TestCase):
 
-    def smoke_test(self):
-        self.assertTrue(1)
+    def test_smoke_test(self):
+        self.assertTrue(2>1)
+
+class UsernameTest(unittest.TestCase):
+
+    def test_complete_username(self):
+        username = "  joshua kneba  "
+        username = str(username).strip().title()
+        self.assertTrue((username),"Joshua Kneba")
+
+    def test_username_numbers(self):
+        username = "123Joshua Kneba"
+        username = not re.search(r"\d", username)
+        self.assertFalse(username)
+
+class QuizQuestionsTest(unittest.TestCase):
+    
+    def test_load_questions(self):
+        questions = load_questions()
+        self.assertIsNotNone(questions)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
