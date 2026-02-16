47e45678b33cce31f02539566f2885269b2a772e
HanJos93 <kneba.j@northeastern.edu>
2026-02-16 16:27:50 +0000

    Added docstrings

--- phishing_quiz.py
+++ phishing_quiz.py
@@ -16,7 +16,9 @@ class PhishingQuiz(tk.Tk):
     def __init__(self, questions):
         super().__init__()
 
-        #Configures the quiz window
+        """
+        Configures the quiz window
+        """
         self.geometry("1920x1080")
         self.title("Buckinghamshire Counil - Phishing 101")
         self.config(bg="#F5F5F5")
@@ -28,21 +30,30 @@ class PhishingQuiz(tk.Tk):
         self.results = tk.IntVar(value=0)
         self.var_results = []
 
-        #Builds and displays the quiz header
+
+        """
+        Builds and displays the quiz header
+        """
         self.quiz_header = tk.Label(self, text="Buckinghamshire Council - Phishing 101", font=("Cabin", 64), bg="#2C2D84", fg="white")
         self.quiz_header.pack(fill="x")
 
-        #Builds and displays the help button
-        self.help_btn = tk.Button(self, command=self.help_messagebox, text="ℹ️", font=("Cabin", 30), anchor="center", justify="center", height=1, width=4)
-        self.help_btn.pack(anchor="nw")
+        """
+        Builds and displays the help button
+        """
+        self.help_button = tk.Button(self, command=self.help_messagebox, text="ℹ️", font=("Cabin", 30), anchor="center", justify="center", height=1, width=4)
+        self.help_button.pack(anchor="nw")
 
-        #Builds and displays the username entry label and input fields
+        """
+        Builds and displays the username entry label and input fields
+        """
         self.username_label = tk.Label(self, text="Please enter your name", font=("Cabin", 30), bg="#F5F5F5", anchor="n", height=2, width=20, padx=10, pady=10)
         self.username_field = tk.Entry(self, textvariable=self.username, font=("Cabin", 20), bg="white")
         self.enter_button = tk.Button(self, command=self.username_validation, text="Enter", font=("Cabin", 20), bg="#F5F5F5", anchor="n", padx=2, pady= 2)
         self.username_entry()
 
-        #Builds the submit button
+        """
+        Builds and displays the submit button
+        """
         self.submit_button = tk.Button(text="Submit", command=self.record_data, font=("Cabin", 20))
 
     #Packs the labels ready to display once the function is called
@@ -51,33 +62,34 @@ class PhishingQuiz(tk.Tk):
         self.username_field.pack(pady=10)
         self.enter_button.pack()
 
-    #Validates the username after the user selects the enter button
+    #Validates the username after the user selects the enter button, then displays the questions if valid
     def username_validation(self):
 
-        self.complete_username = self.username.get().title().strip() #Retrieves the username, removes white spaces and capitalises each word
+        self.complete_username = self.username.get().title().strip() 
+        
+        """
+        The username is retrieved, all words made capital and stripped of empty white spaces before/after the name
+
+        Below the username is checked against various conditions
+        """
 
         try:
-            #Checks if username is empty
             if bool(self.complete_username) == False:
                 messagebox.showerror("Username Error", "Name can not be empty")
                 raise ValueError ("Name can not be empty")
             
-            #Checks if username contains 3 or more characters
             elif len(self.complete_username) <= 2:
                 messagebox.showerror("Username Error", "Name can not be 3 or more characters")
                 raise ValueError ("Name can not be 3 or more characters")
 
-            #Checks if username contains over 30 characters
             elif len(self.complete_username) >= 30:
                 messagebox.showerror("Username Error", "Name can not be over 30 characters")
                 raise ValueError ("Name can not be over 30 characters")
 
-            #Checks if username contains numbers
             elif re.search(r"\d", self.complete_username):
                 messagebox.showerror("Username Error", "Name can not not contain numbers")
                 raise ValueError ("Name should not contain numbers")
             
-            #Checks if username contains special characters aside from hyphens or spaces
             elif not re.fullmatch(r"[a-zA-Z-\s]+", self.complete_username):
                 messagebox.showerror("Username Error", "Name can only contain letters, hyphens or spaces")
                 raise ValueError ("Name should only contain letters, hyphens or spaces")
@@ -89,7 +101,6 @@ class PhishingQuiz(tk.Tk):
             print(err)
             return False
         
-        #Disables the username fields and displays the questions
         self.username_label.destroy()
         self.enter_button.destroy()
         self.username_field.destroy()
@@ -110,14 +121,16 @@ class PhishingQuiz(tk.Tk):
         answers = []
         for var in self.var_results:
             answers.append(var.get())
-        
-        #Checks that no questions are left blank
+         
+        """
+        If no questions are blank, record the results in a csv file then end the quiz
+        """
         empty_question = -1
         if answers.count(empty_question):
             messagebox.showerror("Quiz Error", "All questions must be answered to submit")
             return "All questions must be answered to submit"
 
-        #If no questions are blank, record the results in a csv file then end the quiz
+
         else:
             with open("results.csv", "a", newline="") as results_file:
                 writer = csv.writer(results_file)
@@ -125,12 +138,13 @@ class PhishingQuiz(tk.Tk):
             messagebox.showinfo("Quiz Complete", "Thank you for participating in this quiz, your answers have been submitted")
             self.destroy()
 
-    #Defines the generation of the quiz questions
     def generate_questions(self):
         
         qnumber = 1
 
-        #For each row in quiz_questions.csv create a question label
+        """
+        For each question in quiz_questions.csv it creates a question label, then creates a radio button for each corresponding answer
+        """
         for question in self.questions:
             display_question = tk.Label (self, text=question["questions"], font=("Cabin", 16))
             display_question.pack(anchor="center", padx=40, pady=(20, 5))
@@ -138,7 +152,6 @@ class PhishingQuiz(tk.Tk):
             results = tk.IntVar(value=-1)
             self.var_results.append(results)
 
-            #For each answer create a radio button under their respective question
             option_value = 0
             for option in question["options"]:
                 answer_radiobtns = tk.Radiobutton(self, text=option, variable=results, value=option_value, font=("Cabin", 12))
@@ -147,7 +160,6 @@ class PhishingQuiz(tk.Tk):
 
             qnumber += 1
         
-        #Displays the submit button 
         self.submit_button.pack(anchor="s", padx=30)
             
 
